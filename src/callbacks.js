fc41375e672a0955a70805ce8523126685390fe2
Adrian Miu <florin_miu@yahoo.com>
2013-07-25 15:50:00 +0300

    Implemented the add()/remove() methods with tests

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,5 +1,40 @@
 (function(root, undefined){
+	"use strict";
+
+	// utility function to compare 2 callbacks
+	var __compareCallbacks = function(c1, c2) {
+		if (c1.priority < c2.priority) {
+			return -1;
+		} else if (c1.priority > c2.priority) {
+			return 1;
+		}
+		return 0;
+	};
+	
+	// method to reorder the callbacks based on their priorities
+	// will be called after an callback is added to the registry
+	var __updateCallbacksOrder = function() {
+		this._registry.sort(__compareCallbacks);
+	};
+	
+	// function to execute a callback (an object from the callbacks registry)
+	var __executeCallback = function(clbk, data) {
+		context = clbk.context || this;
+		return clkb.call(context, data);
+	};
+	
+	// if a callback throws this exception the rest of the callbacks will not be executed
+	root.CallbacksBreakExecutionException = function(message) {
+		this.message = message; 
+		this.stack = Error().stack;
+	}
+	root.CallbacksBreakExecutionException.prototype =  new Error;
+	
 	root.Callbacks = function(flags) {
+		// flags are options that alter the behaviour of the callback
+		// unique: does not allow adding the same function to the registry
+		// once: the callbacks are executed only once. Any subsequent calls will not be executed
+		// memory: store all the results
 		flags = (flags + '' === flags) 
 			? flags.split(' ') 
 		    : (flags instanceof Array) 
@@ -7,21 +42,82 @@
 		    	: ['unique'];
 		this.hasFlag = function (flag) {
 			return flags.indexOf(flag) !== -1;
-		}
+		};
+		
+		// variable used to determine what is the next priority to be assigned
+		// by default to a callback, in case one was not provided
+		var priority = 0;
+		this.getCurrentPriority = function() {
+			return priority++;
+		};
+		
+		// registry of all the callbacks
+		this._registry = [];
+		// method to check if the callback is already in the registry
+		this.contains = function(func) {
+			var len = this._registry.length, i=len;
+			while (i--) {
+				if (this._registry[i]['callback'] === func) {
+					return true;
+				}
+			}
+			return false;
+		};
 		return this;
-	}
+	};
 	// callbacks prototype
 	var CP = Callbacks.prototype;
 	// add a function to the callback list
-	CP.on = function(name, func, priority, context) {}
+	CP.add = function(func, context, priority) {
+		priority = priority || this.getCurrentPriority();
+		var entry = {
+			callback: func,
+			context: context,
+			priority: priority
+		};
+		if (!this.hasFlag('unique') || !this.contains(func)) { 
+			this._registry.push(entry);
+			__updateCallbacksOrder.call(this);
+		}
+		return this;
+	};
 	// add a function tot he callback list making sure it is executed only once
-	CP.one = function(name, func, priority, context) {}
+	CP.remove = function(func) {
+		var len = this._registry.length, i=len;
+		while (i--) {
+			if (this._registry[i]['callback'] === func) {
+				this._registry.splice(i, 1);
+			}
+		}
+		return this;
+	};
 	// removes a function from the callback list
-	CP.off = function(name, func) {}
+	CP.fire = function(data) {
+		return this.fireWith(data, null);
+	};
 	// trigger the execution of a callback list
-	CP.trigger = function(name, data) {
-		this.triggerWith(name, data, this);
-	}
-	// trigger the execution of a callback list within a certain context
-	CP.triggerWith = function(name, data, context) {}
+	CP.fireWith = function(data, context) {
+		if (this.hasFlag('once')) {
+			return this._lastResult;
+		}
+		_results = [];
+		// keep the last called context for 'once' type of callbacks
+		this._lastContext = context;
+		var len = this._registry.length, i=len;
+		while (i--) {
+			try {
+				results.push(__executeCallback(this._registry[i], data));
+			} catch (e) {
+				if (e instanceof CallbacksBreakExecutionException) {
+					break;
+				}
+				throw e;
+			}
+		}
+		if (this.hasFlag('memory')) {
+			this._results = _results;
+		}
+		this._lastResult = _results[_results.length - 1];
+		return this._lastResult;
+	};
 })(window, undefined);
--- tests/tests.js
+++ tests/tests.js
@@ -5,7 +5,7 @@ test( "Callbacks exists in the global namespace", function() {
 test ('Callbacks can be created', function() {
 	var callbacks = new Callbacks();
 	ok(callbacks instanceof Object, 'Callback was created');
-	['on', 'off', 'trigger', 'on'].forEach(function(element, index) {
+	['add', 'remove', 'fire', 'fireWith'].forEach(function(element, index) {
 		ok(callbacks[element] instanceof Function, 'method ' + element + '() exits');
 	});
 });
@@ -24,7 +24,73 @@ test ('Flags can be passed as array', function() {
 	ok(!clbs.hasFlag('flagC'), 'callback has flagC');
 });
 
-test ('Default flag is "unique"', function() {
+test('Default flag is "unique"', function() {
 	var clbs = new Callbacks();
 	ok(clbs.hasFlag('unique'), 'callback has the "unique" flag');
 });
+
+test('Priority is generated correctly', function() {
+	var clbs = new Callbacks('unique');
+	equal(0, clbs.getCurrentPriority(), 'First priority is 0');
+	equal(1, clbs.getCurrentPriority(), 'Second priority is 1');
+	equal(2, clbs.getCurrentPriority(), 'Third priority is 2');
+});
+
+test('Callbacks are added to the stack', function() {
+	var clbs = new Callbacks('unique');
+	var func = function() {},
+		context = {},
+		found = 0;
+	clbs.add(func, context);
+	ok (clbs.contains(func), 'callback is detected as being in the stack');
+	
+	// add again to test if
+	clbs.add(func, context);
+	clbs._registry.forEach(function(clbk, index) {
+		if (clbk.callback == func) {
+			found++;
+		}
+	});
+	equal (found, 1, 'callback was added to the stack');
+});
+
+test('Callbacks are added to the stack multiple times', function() {
+	var clbs = new Callbacks('multiple');
+	var func = function() {},
+		context = {},
+		found = 0;
+	clbs.add(func, context);
+	clbs.add(func, context);
+	clbs._registry.forEach(function(clbk, index) {
+		if (clbk.callback == func) {
+			found++;
+		}
+	});
+	equal (found, 2, 'callback was added to the stack 2 times');
+});
+
+test('Callbacks are added in the proper order', function() {
+	var clbs = new Callbacks('unique');
+	var	funcA = function() { return 'A'; },
+		funcB = function() { return 'B'; },
+		funcC = function() { return 'C'; },
+		context = {};
+	clbs.add(funcA, context); // with default priority (ie: the available one)
+	clbs.add(funcB, context, -999); // to be executed first
+	clbs.add(funcC, context, -10); // to be executed second
+	console.log(clbs);
+	equal(clbs._registry[0]['callback'], funcB, 'first callback is funcB');
+	equal(clbs._registry[1]['callback'], funcC, 'second callback is funcC');
+	equal(clbs._registry[2]['callback'], funcA, 'third callback is funcA');
+});
+
+test('Callbacks are removed from the stack', function() {
+	var clbs = new Callbacks('multiple');
+	var func = function() {},
+		context = {};
+	clbs.add(func, context);
+	clbs.add(func, context);
+	clbs.remove(func);
+	ok (!clbs.contains(func), 'callback was removed');	
+});
+
